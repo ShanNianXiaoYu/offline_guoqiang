0a356d4b2446e578c88c77c190205af954ac628e
ShanNianXiaoYu <2957498137@qq.com>
2025-03-24 20:44:14 +0800

    Initial commit

---
+++ shell/quanbu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python xxx.py -d dev_realtime_v1_guoqiang_zhang -t order_info
+python xxx.py -d dev_realtime_v1_guoqiang_zhang -t activity_info
+python xxx.py -d dev_realtime_v1_guoqiang_zhang -t activity_order
+python xxx.py -d dev_realtime_v1_guoqiang_zhang -t activity_rule
+python xxx.py -d dev_realtime_v1_guoqiang_zhang -t activity_sku
+python xxx.py -d dev_realtime_v1_guoqiang_zhang -t base_category1
+python xxx.py -d dev_realtime_v1_guoqiang_zhang -t base_category2
+python xxx.py -d dev_realtime_v1_guoqiang_zhang -t base_category3
